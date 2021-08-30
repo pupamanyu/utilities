2b52511e32aa10e8ec05ca8b3b6172521580483a
Pramod Upamanyu <pupamanyu@users.noreply.github.com>
2021-08-30 12:39:39 -0700

    Update td2bq-schema.sh

--- td2bq-schema.sh
+++ td2bq-schema.sh
@@ -1,1 +1,1 @@
-for field in `cat schema.json | jq -c .Fields | jq -c '.[]'`; do echo $field | jq -r -c '."@name"'; echo $field | jq -r -c '."DataType"|to_entries[]|.key' | tr [a-z] [A-Z]; done
+cat schema.xml | xq -c '.TeradataDBObjectSet.Table.ColumnList.Column'| jq -c '.[]' | jq -r -c '[."@name", ((."DataType"|to_entries[]).key | ascii_upcase)] | @csv'
