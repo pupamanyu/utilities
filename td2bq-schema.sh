f3e1a93ae2a7091d7a0f87f6d7fd657b250eacec
Pramod Upamanyu <pupamanyu@users.noreply.github.com>
2021-08-30 12:40:30 -0700

    Update td2bq-schema.sh

--- td2bq-schema.sh
+++ td2bq-schema.sh
@@ -1,1 +1,2 @@
+# schema.xml contains the output of SHOW IN XML <TABLE> statement
 cat schema.xml | xq -c '.TeradataDBObjectSet.Table.ColumnList.Column'| jq -c '.[]' | jq -r -c '[."@name", ((."DataType"|to_entries[]).key | ascii_upcase)] | @csv'
